0d9d6e84b2c2129742b45d9ee8a8bb828303357a
SaiKrishnaThalla <59231854+thallasaikrishna@users.noreply.github.com>
2021-12-19 14:51:32 +0530

    Update srcipt.js

--- srcipt.js
+++ srcipt.js
@@ -1,73 +1,64 @@
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap');  
-*{  
-  box-sizing: border-box;  
-}  
-body{  
-  margin: 0;  
-  font-family: "Poppins", sans-serif;  
- background-color: rgb(181, 194, 135);  
- color: #eee;  
- display: flex;  
- align-items: center;  
- justify-content: center;  
- min-height: 100vh;  
-  }  
-  .pw-container{  
-    width: 400px;  
-    background-color: rgb(36, 202, 64);  
-    box-shadow: 0 4px 10px rgba(0,0,0,0.6);  
-  }  
-   .pw-header{  
-    padding: 1rem;  
-   }  
- .pw{  
-   width: 100%;  
-   height: 70px;  
-   background-color: rgb(72, 214, 219);  
-   display: flex;  
-   align-items: center;  
-   position: relative;  
-   font-size: 1.5rem;  
-   padding: 1rem;  
-   /* overflow: auto; */  
+const PwEl = document.getElementById("pw");  
+ const copyEl = document.getElementById("copy");  
+ const lenEl = document.getElementById("len");  
+ const upperEl = document.getElementById("upper");  
+ const lowerEl = document.getElementById("lower");  
+ const symbolEl = document.getElementById("symbol");  
+ const generateEl = document.getElementById("generate");  
+ const numberEl = document.getElementById("number");  
+ const upperLetters = "ABCDEFGHIJKLMNOPQSRTUVWXYZ";  
+ const lowerLetters = "abcdefghijklmnopqrstuvwxyz";  
+ const numbers = "0123456789";  
+ const symbol = "~!@#$%^&*()_+=|";  
+ function getLowercase() {  
+  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];  
  }  
- .pw button{  
-   position: absolute;  
-   top: 0;  
-   right: 0;  
-   transform: translate(0, 50%);  
-   transition: opacity 0.2s ease, transform 0.2s ease;  
-   opacity: 0;  
-   background-color: rgb(28, 252, 21);  
-   padding: 0.25rem 1rem;  
-   font-family: inherit;  
-   font-weight: bold;  
-   color: #fff;  
-   border: none;  
-   cursor: pointer;  
-  }  
-  .pw:hover button{  
-    opacity: 1;  
-    transform: translate(0,-80%);  
-  }  
-   .pw-body{  
-    padding: 0 1rem 1rem;  
-   }  
- .form-control{  
-   color: #eee;  
- display: flex;  
- justify-content: space-between;  
- margin: 0.75rem 0;  
+ function getUppercase() {  
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)];  
  }  
- .generate{  
-   display: block;  
-   background-color: #ecb602;  
-   color: rgb(70, 29, 255);  
-   font-weight: bold;  
-   padding: 1rem;  
-   font-size: 1.5rem;  
-   margin-top: 1rem;  
-   border: none;  
-   width: 100%;  
-   cursor: pointer;  
+ function getNumber() {  
+  return numbers[Math.floor(Math.random() * numbers.length)];  
  }  
+ function getSymbol() {  
+  return symbol[Math.floor(Math.random() * symbol.length)];  
+ }  
+ function generatePassword() {  
+  const len = lenEl.value;  
+  let password = "";  
+  for (let i = 0; i < len; i++) {  
+   const x = generateX();  
+   password += x;  
+  }  
+  PwEl.innerText = password;  
+ }  
+ function generateX() {  
+  const xs = [];  
+  if (upperEl.checked) {  
+   xs.push(getUppercase());  
+  }  
+  if (lowerEl.checked) {  
+   xs.push(getLowercase());  
+  }  
+  if (numberEl.checked) {  
+   xs.push(getNumber());  
+  }  
+  if (symbolEl.checked) {  
+   xs.push(getSymbol());  
+  }  
+  if (xs.length === 0) return "";  
+  return xs[Math.floor(Math.random() * xs.length)];  
+ }  
+ generateEl.addEventListener("click", generatePassword);  
+ copyEl.addEventListener("click", () => {  
+  const textarea = document.createElement("textarea");  
+  const password = PwEl.innerText;  
+  if (!password) {  
+   return;  
+  }  
+  textarea.value = password;  
+  document.body.appendChild(textarea);  
+  textarea.select();  
+  document.execCommand("copy");  
+  textarea.remove();  
+  alert("password copied to clipboard");  
+ }); 
